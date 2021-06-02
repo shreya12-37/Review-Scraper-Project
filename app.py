d025e254e433ebd8a992e493d827337d9d5c2fe9
Rishab Goyal <rishabgoyal432@gmail.com>
2021-06-02 23:46:46 +0530

    Requests Sccessful|| Changed to Get

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, request
-from flask_cors import CORS
+from flask import Flask, request, make_response, jsonify, send_from_directory , send_file
+from flask_cors import CORS ,cross_origin
 import subprocess
 import amazon_reviews
 import amazon_reviews1
@@ -8,17 +8,22 @@ from bs4 import BeautifulSoup
 
 configs = {
     "ORIGINS": [
-        "*",
+        "http://localhost:3000/",
+        "http://127.0.0.1:3000/",
+        "https://review-scraper-app.herokuapp.com",
+        "*"
     ],
-    "SECRET_KEY": "**********",
+    "SECRET_KEY": "Hello World",
 }
 app = Flask(__name__)
 app.secret_key = configs['SECRET_KEY']
-CORS(app, resources={ r'/*': {'origins': configs['ORIGINS']}}, supports_credentials=True)
-
-@app.route("/download-csv-file", methods=["POST"])
+# CORS(app, resources={ r"/*": {'origins': configs['ORIGINS']}}, supports_credentials=True)
+CORS(app, supports_credentials=True)
+app.config["CLIENT_CSV"] = "H:/Web/Projects/Review Scrapper/Review-Scraper-Project"
+@app.route("/download-csv-file", methods=["GET"])
+# @cross_origin(headers=["Access-Control-Allow-Origin","Access-Control-Allow-Headers","Access-Control-Allow-Methods"])
 def scrape():
-    data = request.form.to_dict()
+    data = request.args.to_dict()
     url = data["url"]
     pages = data["pages"]
     cmd = (
@@ -28,19 +33,24 @@ def scrape():
         + pages
     )
     process = subprocess.run(cmd)
-    # process = subprocess.run(f'scrapy runspider amazon_reviews1.py -o output.csv')
-    return "hello"
+    try:
+        return send_file("H:/Web/Projects/Review Scrapper/Review-Scraper-Project/output.csv", as_attachment=True)
+    except FileNotFoundError:
+        # abort(404)
+        return "response"
+    
 
 
-@app.route("/download-summary", methods=["POST"])
+@app.route("/download-summary", methods=["GET"])
+# @cross_origin(origin='http://localhost:3000/',headers=['Access-Control-Allow-Origin','Content-Type','Authorization'])
 def summary():
-    data = request.form.to_dict()
+    data = request.args.to_dict()
     my_url = data["url"]
     r = requests.get(my_url)
     htmlcontent = r.content
     soup = BeautifulSoup(htmlcontent, "html.parser")
     product_title = soup.find(class_=["a-text-ellipsis"]).get_text()
-    print(product_title)
+    # print(product_title)
     overall_rating = soup.find(class_=["averageStarRating"]).get_text()
     global_rating_reviews = (
         soup.find(class_=["a-row a-spacing-base a-size-base"]).get_text().strip()
@@ -61,7 +71,11 @@ def summary():
         "two stars": two_star,
         "one stars": one_star,
     }
-    return summary
+    response = jsonify(message=summary)
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add('Access-Control-Allow-Headers', "*")
+    response.headers.add('Access-Control-Allow-Methods', "*")
+    return response
 
 
 if __name__ == "__main__":
